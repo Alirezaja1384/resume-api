6b05431bc5c46f5e75127af4954bb6db2d46d6a8
Alireza Jafari <alirezaja1384@gmail.com>
2024-03-29 14:27:29 +0330

    `liara.yml` workflow improved

---
+++ generate-requirements.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+BASE_DIR="$(dirname "$0")"
+
+echo "> Syncing requirements.txt ..."
+# Generate requirements.txt (windows-specific packages are excluded)
+poetry -C "$BASE_DIR" export --format requirements.txt --only main |
+    grep -v -i win \
+        >"${BASE_DIR}/resume_api/requirements.txt"
+
+echo "> Requirements exported successfully!"
