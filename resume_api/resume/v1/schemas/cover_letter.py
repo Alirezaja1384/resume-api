bcf02f17928ba148b89b4b38e1370e1323ad7397
Alireza Jafari <alirezaja1384@gmail.com>
2024-03-29 17:08:17 +0330

    DetailedCoverLetterSerializer: user field bug fixed.
    Details:
     - Custom serializer provided for `user` field.

--- resume_api/resume/v1/schemas/cover_letter.py
+++ resume_api/resume/v1/schemas/cover_letter.py
@@ -1,11 +1,14 @@
 from rest_framework.serializers import ModelSerializer
 
+from authentication.v1.schemas import UserSerializer
 from resume.models import CoverLetter
 from .project import ProjectSerializer
 from .work_experience import WorkExperienceSerializer
 
 
 class DetailedCoverLetterSerializer(ModelSerializer):
+    user = UserSerializer()
+
     projects = ProjectSerializer(many=True)
 
     work_experiences = WorkExperienceSerializer(many=True)
